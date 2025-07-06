908bdbee40e9cf7a7e08674c40549dd40adb3a59
Gabriel <gabriel.c.silva2@outlook.com>
2025-07-06 15:36:47 -0300

    Backup Neovim config

---
+++ nvim/lua/config/remap.lua
@@ -0,0 +1,8 @@
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.keymap.set('n', '<leader>n', ':Neotree toggle filesystem left<CR>')
+
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-context"
+  },
+  priority = 1000,
+  config = function()
+    require("nvim-treesitter.configs").setup {
+      ensure_installed = {
+        "c", "cpp", "lua", "markdown", "markdown_inline",
+        "python", "rust", "javascript", "vim", "vimdoc", "zig"
+      },
+      auto_install = true,
+      sync_install = false,
+      highlight = { enable = true },
+    }
+  end
+}
+
