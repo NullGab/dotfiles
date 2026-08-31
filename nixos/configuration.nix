57ed4c7c428321212c3e0b00ebca9b6fcd51ed28
Gabriel <gabriel.c.silva2@outlook.com>
2026-08-31 01:46:12 -0300

    Auto-update: 2026-08-31 01:46:12

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,131 +1,137 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Use the systemd-boot EFI boot loader.
+# =========================================
+# Boot & System
+# =========================================
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-   networking.hostName = "nixos"; # Define your hostname.
-	   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # Configure network connections interactively with nmcli or nmtui.
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "26.05";
+
+# =========================================
+# Networking, Time & Locale
+# =========================================
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-   time.timeZone = "America/Sao_Paulo";
+  time.timeZone = "America/Sao_Paulo";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-# Enable Zsh system-wide
-programs.zsh.enable = true;
-nixpkgs.config.allowUnfree = true;
-fonts.packages = with pkgs; [
-  nerd-fonts.jetbrains-mono
-    nerd-fonts.meslo-lg
-    nerd-fonts.caskaydia-cove
-];
-
-
-  # Enable the Steam module
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  console = {
+    font = "Lat2-Terminus16";
+    useXkbConfig = true; 
   };
 
-  # Enable Flatpak and Flathub integration
-  services.flatpak.enable = true;
+# =========================================
+# Desktop Environment & Display (X11)
+# =========================================
+  services.xserver = {
+    enable = true;
+    desktopManager.cinnamon.enable = true;
+    displayManager.lightdm = {
+      enable = true;
+      background = ./yotsubato.jpeg; 
+    };
+    xkb = {
+      layout = "br,us";
+      options = "grp:alt_shift_toggle";    
+    };
+  };
 
-  # Define your declarative Flatpaks
-  services.flatpak.packages = [
-    "com.obsproject.Studio"
-    "org.vinegarhq.Sober"
+# =========================================
+# Hardware & Peripherals
+# =========================================
+  services.libinput.enable = true; # Mouse & Touchpad support
+
+    services.keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = ["*"];
+          settings = {
+            main = {
+              capslock = "esc";
+              esc = "capslock";
+            };
+          };
+        };
+      };
+    };
+
+# =========================================
+# Audio (PipeWire)
+# =========================================
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true; 
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true; 
+  };
+
+# =========================================
+# Software & Packages
+# =========================================
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim 
+      wget
+      neovim
+      kitty
+      btop
+      keepassxc
+      git
+      nodejs
+      flatpak
+      zsh
+      oh-my-zsh
   ];
-  # Select internationalisation properties.
- i18n.defaultLocale = "en_US.UTF-8";
- console = {
-   font = "Lat2-Terminus16";
-   useXkbConfig = true; # use xkb.options in tty.
- };
 
- services.xserver = {
-	 enable = true;
-   displayManager.lightdm = {
-     enable = true;
-     background = ./yotsubato.jpeg; 
-   };
-	 desktopManager.cinnamon.enable = true;
- };
-
- # Moved libinput to top-level to fix deprecation warning
- services.libinput.enable = true;
-
- # Enable nix-ld so Mason can run pre-compiled LSP binaries
- programs.nix-ld.enable = true;
-
-  # Configure keymap in X11
-   services.xserver.xkb= {
-   layout = "br,us";
-   options = "grp:alt_shift_toggle";	
-   };
-   services.keyd = {
-	   enable = true;
-	   keyboards={
-		   default = {
-			   ids=["*"];
-			   settings={
-				   main = {
-					   capslock = "esc";
-					   esc = "capslock";
-				   };
-			   };
-		   };
-	   };
-   };
-
-   hardware.pulseaudio.enable = false;
-
-security.rtkit.enable = true; # Required for PipeWire to request realtime scheduling
-services.pipewire = {
-  enable = true;
-  alsa.enable = true;
-  alsa.support32Bit = true;
-  pulse.enable = true; # This allows PipeWire to act as the PulseAudio server
-};
-
-# Your main user block around line 82
-users.users.gabriel = {
-  isNormalUser = true;
-  description = "Gabriel";
-  extraGroups = [ "networkmanager" "wheel" ];
-  shell = pkgs.zsh; # <-- Add this line here
-  packages = with pkgs; [
-    # your packages
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+      nerd-fonts.meslo-lg
+      nerd-fonts.caskaydia-cove
   ];
-};
-   programs.firefox.enable = true;
 
- environment.systemPackages = with pkgs; [
-   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-   wget
-   neovim
-   kitty
-   btop
-   keepassxc
-   git
-   nodejs
-   flatpak
-   zsh
-   oh-my-zsh
- ];
+# =========================================
+# Programs & Services Configs
+# =========================================
+  programs = {
+    zsh.enable = true;
+    firefox.enable = true;
+    nix-ld.enable = true; # Run pre-compiled LSP binaries
 
+      steam = {
+        enable = true;
+        remotePlay.openFirewall = true; 
+        dedicatedServer.openFirewall = true; 
+      };
+  };
 
-  system.stateVersion = "26.05"; # Did you read the comment?
+  services.flatpak = {
+    enable = true;
+    packages = [
+      "com.obsproject.Studio"
+        "org.vinegarhq.Sober"
+    ];
+  };
 
+# =========================================
+# Users
+# =========================================
+  users.users.gabriel = {
+    isNormalUser = true;
+    description = "Gabriel";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh; 
+    packages = with pkgs; [];
+  };
 }
